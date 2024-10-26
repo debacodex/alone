cccbd97255756c5f9e27e32ef21126bc5e046cfd
debacodex <debashissabar0@gmail.com>
2024-10-26 15:27:46 +0530

    update

---
+++ app/src/main/java/com/github/debashis/ui/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.github.debashis.ui.activity;
+
+import android.os.Bundle;
+import com.github.debashis.R;
+import com.github.debashis.common.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity { 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+}
+/*don't forget to subscribe my YouTube channel for more Tutorial and mod*/
+/*
+https://youtube.com/channel/UC_lCMHEhEOFYgJL6fg1ZzQA */
